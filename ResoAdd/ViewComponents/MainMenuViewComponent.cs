6dc932e06931c569aa2a798a17d949b8b99fa6d7
ogyreck <andrej.peredvigin@mail.ru>
2023-11-04 16:38:00 +0300

    Добавление динамического главного меню

---
+++ ResoAdd/ViewComponents/MainMenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResoAdd.BL.Auth;
+
+namespace ResoAdd.ViewComponents
+{
+	public class MainMenuViewComponent: ViewComponent
+	{
+		private readonly ICurrentUser _currentUser;
+        public MainMenuViewComponent(ICurrentUser currentUser)
+        {
+			_currentUser = currentUser;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+		{
+			bool isLoggedIn = await _currentUser.ISLoggedIn();
+			return View("Index", isLoggedIn);
+		}
+	}
+}
